b061fad12b039dc921b9667fe67d5ce3729128dc
ilyarolf <ilyarolf@mail.ru>
2024-10-15 19:37:52 +0300

    deposit model fixes

--- models/deposit.py
+++ models/deposit.py
@@ -9,7 +9,6 @@ class Deposit(Base):
     id = Column(Integer, primary_key=True)
     tx_id = Column(String, nullable=False, unique=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    user = relationship("User", backref=backref("users",lazy="joined"))
     network = Column(String, nullable=False)
     token_name = Column(String, nullable=True)
     amount = Column(BigInteger, nullable=False)
