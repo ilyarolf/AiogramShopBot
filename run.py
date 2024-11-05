e2eb47329479295249797b4d77f691f41aa1e186
ilyarolf <ilyarolf@mail.ru>
2024-11-06 00:14:35 +0300

    localizator.py refactored

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ from handlers.user.all_categories import all_categories_router
 from handlers.user.my_profile import my_profile_router
 from services.user import UserService
 from utils.custom_filters import IsUserExistFilter
-from utils.localizator import Localizator
+from utils.localizator import Localizator, BotEntity
 
 logging.basicConfig(level=logging.INFO)
 main_router = Router()
@@ -20,11 +20,11 @@ main_router = Router()
 
 @main_router.message(Command(commands=["start", "help"]))
 async def start(message: types.message):
-    all_categories_button = types.KeyboardButton(text=Localizator.get_text_from_key("all_categories"))
-    my_profile_button = types.KeyboardButton(text=Localizator.get_text_from_key("my_profile"))
-    faq_button = types.KeyboardButton(text=Localizator.get_text_from_key("faq"))
-    help_button = types.KeyboardButton(text=Localizator.get_text_from_key("help"))
-    admin_menu_button = types.KeyboardButton(text=Localizator.get_text_from_key("admin_menu"))
+    all_categories_button = types.KeyboardButton(text=Localizator.get_text(BotEntity.USER, "all_categories"))
+    my_profile_button = types.KeyboardButton(text=Localizator.get_text(BotEntity.USER, "my_profile"))
+    faq_button = types.KeyboardButton(text=Localizator.get_text(BotEntity.USER, "faq"))
+    help_button = types.KeyboardButton(text=Localizator.get_text(BotEntity.USER, "help"))
+    admin_menu_button = types.KeyboardButton(text=Localizator.get_text(BotEntity.ADMIN, "menu"))
     user_telegram_id = message.chat.id
     user_telegram_username = message.from_user.username
     is_exist = await UserService.is_exist(user_telegram_id)
@@ -37,20 +37,21 @@ async def start(message: types.message):
     if user_telegram_id in config.ADMIN_ID_LIST:
         keyboard.append([admin_menu_button])
     start_markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2, keyboard=keyboard)
-    await message.answer(Localizator.get_text_from_key("start_message"), reply_markup=start_markup)
+    await message.answer(Localizator.get_text(BotEntity.COMMON, "start_message"), reply_markup=start_markup)
 
 
-@main_router.message(F.text == Localizator.get_text_from_key("faq"), IsUserExistFilter())
+@main_router.message(F.text == Localizator.get_text(BotEntity.USER, "faq"), IsUserExistFilter())
 async def faq(message: types.message):
-    await message.answer(Localizator.get_text_from_key("faq_string"))
+    await message.answer(Localizator.get_text(BotEntity.USER, "faq_string"))
 
 
-@main_router.message(F.text == Localizator.get_text_from_key("help"), IsUserExistFilter())
+@main_router.message(F.text == Localizator.get_text(BotEntity.USER, "help"), IsUserExistFilter())
 async def support(message: types.message):
     admin_keyboard_builder = InlineKeyboardBuilder()
 
-    admin_keyboard_builder.button(text=Localizator.get_text_from_key("help_button"), url=SUPPORT_LINK)
-    await message.answer(Localizator.get_text_from_key("help_string"), reply_markup=admin_keyboard_builder.as_markup())
+    admin_keyboard_builder.button(text=Localizator.get_text(BotEntity.USER, "help_button"), url=SUPPORT_LINK)
+    await message.answer(Localizator.get_text(BotEntity.USER, "help_string"),
+                         reply_markup=admin_keyboard_builder.as_markup())
 
 
 main_router.include_router(admin_router)
