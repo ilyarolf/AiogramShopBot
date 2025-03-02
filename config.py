79dc029226adec7c61de84cbba73ec94da6d17f0
ilyarolf <ilyarolf@mail.ru>
2025-03-02 13:19:07 +0300

    implementing fastapi with webhook secret key

--- config.py
+++ config.py
@@ -29,3 +29,4 @@ BOT_LANGUAGE = os.environ.get("BOT_LANGUAGE")
 MULTIBOT = os.environ.get("MULTIBOT", False) == 'true'
 ETHPLORER_API_KEY = os.environ.get("ETHPLORER_API_KEY")
 CURRENCY = Currency(os.environ.get("CURRENCY"))
+WEBHOOK_SECRET_TOKEN = os.environ.get("WEBHOOK_SECRET_TOKEN")
