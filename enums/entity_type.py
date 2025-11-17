2762a1c8e77e0496e021205799d529fee4003cef
ilyarolf <ilyarolf@mail.ru>
2025-11-17 21:59:34 +0300

    moved enums from callbacks

---
+++ enums/entity_type.py
@@ -0,0 +1,19 @@
+from enum import IntEnum
+
+from enums.bot_entity import BotEntity
+from utils.localizator import Localizator
+
+
+class EntityType(IntEnum):
+    CATEGORY = 1
+    SUBCATEGORY = 2
+    ITEM = 3
+
+    def get_localized(self):
+        match self:
+            case EntityType.CATEGORY:
+                return Localizator.get_text(BotEntity.COMMON, "category")
+            case EntityType.SUBCATEGORY:
+                return Localizator.get_text(BotEntity.COMMON, "subcategory")
+            case EntityType.ITEM:
+                return Localizator.get_text(BotEntity.COMMON, "item")
