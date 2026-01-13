5ef9a18847b45cfec1b82d9003ff320b05e46ce8
ilyarolf <ilyarolf@mail.ru>
2026-01-13 18:40:35 +0300

    update config files, set default value for variables

--- config.py
+++ config.py
@@ -12,25 +12,25 @@ if RUNTIME_ENVIRONMENT == RuntimeEnvironment.DEV:
     WEBHOOK_HOST = start_ngrok()
 else:
     WEBHOOK_HOST = get_sslipio_external_url()
-WEBHOOK_PATH = os.environ.get("WEBHOOK_PATH")
-WEBAPP_HOST = os.environ.get("WEBAPP_HOST")
-WEBAPP_PORT = int(os.environ.get("WEBAPP_PORT"))
+WEBHOOK_PATH = os.environ.get("WEBHOOK_PATH", "/")
+WEBAPP_HOST = os.environ.get("WEBAPP_HOST", "0.0.0.0")
+WEBAPP_PORT = int(os.environ.get("WEBAPP_PORT", "5000"))
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 TOKEN = os.environ.get("TOKEN")
 ADMIN_ID_LIST = os.environ.get("ADMIN_ID_LIST").split(',')
 ADMIN_ID_LIST = [int(admin_id) for admin_id in ADMIN_ID_LIST]
 SUPPORT_LINK = os.environ.get("SUPPORT_LINK")
 DB_ENCRYPTION = os.environ.get("DB_ENCRYPTION", False) == 'true'
-DB_NAME = os.environ.get("DB_NAME")
+DB_NAME = os.environ.get("DB_NAME", "database.db")
 DB_PASS = os.environ.get("DB_PASS")
-PAGE_ENTRIES = int(os.environ.get("PAGE_ENTRIES"))
+PAGE_ENTRIES = int(os.environ.get("PAGE_ENTRIES", "8"))
 MULTIBOT = os.environ.get("MULTIBOT", False) == 'true'
-CURRENCY = Currency(os.environ.get("CURRENCY"))
+CURRENCY = Currency(os.environ.get("CURRENCY", "USD"))
 KRYPTO_EXPRESS_API_KEY = os.environ.get("KRYPTO_EXPRESS_API_KEY")
 KRYPTO_EXPRESS_API_URL = os.environ.get("KRYPTO_EXPRESS_API_URL")
 KRYPTO_EXPRESS_API_SECRET = os.environ.get("KRYPTO_EXPRESS_API_SECRET")
 WEBHOOK_SECRET_TOKEN = os.environ.get("WEBHOOK_SECRET_TOKEN")
-REDIS_HOST = os.environ.get("REDIS_HOST")
+REDIS_HOST = os.environ.get("REDIS_HOST", "redis")
 REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
 # VARIABLES FOR CRYPTO FORWARDING
 CRYPTO_FORWARDING_MODE = os.environ.get("CRYPTO_FORWARDING_MODE", False) == 'true'
